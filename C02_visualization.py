9ef7c8fb73fd4fb071f64b6d26d496d92f46fdbf
kusin <aryajayaalamsyah@gmail.com>
2024-07-04 20:22:58 +0700

    -

--- C02_visualization.py
+++ C02_visualization.py
@@ -27,7 +27,26 @@ def lineplot2(x1, y1, label1, x2, y2, label2, title):
   # membuat time series plot
   fig, ax = plt.subplots(figsize = (8,4))
   ax.plot(x1, y1, color="tab:blue", label=label1, linewidth=2.5)
-  ax.plot(x2, y2, color="tab:red", label=label2, linewidth=2.5)
+  ax.plot(x2, y2, color="tab:orange", label=label2, linewidth=2.5)
+
+  # membuat label-label
+  ax.set_title(title, fontsize=12)
+  ax.set_xlabel("", fontsize=12)
+  ax.set_ylabel("", fontsize=12)
+  ax.legend(loc="best")
+  ax.grid(True)
+
+  # return values
+  return plt.show()
+# --------------------------------------------------------------
+
+# visualisasi timeseries plot
+def lineplot3(x1, y1, label1, x2, y2, label2, title):
+
+  # membuat time series plot
+  fig, ax = plt.subplots(figsize = (8,4))
+  ax.plot(x1, y1, color="tab:blue", label=label1, linewidth=2.5, linestyle="solid")
+  ax.plot(x2, y2, color="tab:red", label=label2, linewidth=2.5, linestyle="dashed")
 
   # membuat label-label
   ax.set_title(title, fontsize=12)
